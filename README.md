45cfac11b4db130b02d7b6006d73da6644156734
clifflyc <clifflyc@gmail.com>
2021-09-09 16:49:54 -0400

    Setup Unity Package

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GamePlugin-Subsystem
+
+This repo structured as a custom package for Unity.
+
+To install it, use the "Add package from git url..." option in Unity Package Manager.
+To develop it, clone the repo, and add it into any project using "Add package from disk..." option in Unity Package Manager; Unity will directly install from the cloned repo and will see any changes made to it there.
---
+++ Runtime/RealmAgent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+namespace RealmAI {
+    public class RealmAgent : Agent {
+        private List<int> _stuff = new List<int>();
+        
+        private void Awake() {
+            Debug.Log("Hello World!");
+        }
+    
+        public override void OnEpisodeBegin() {
+        }
+    
+        public override void CollectObservations(VectorSensor sensor) {
+        }
+    
+        public override void OnActionReceived(ActionBuffers actions) {
+        }
+    
+        public override void Heuristic(in ActionBuffers actionsOut) {
+        }
+    }
+}
